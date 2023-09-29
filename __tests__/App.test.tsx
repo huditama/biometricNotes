8697446c7d22e8421d2f89077f8d87bfddf08076
Reyhan Huditama <reyhan21098@gmail.com>
2023-09-29 17:40:05 +0700

    featL init testing

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,6 @@
-/**
- * @format
- */
-
-import 'react-native';
-import React from 'react';
-
-// Note: import explicitly to use the types shiped with jest.
-import { it } from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import App from '../src/app/App';
+import { Authenticate } from '../src/screens/Authenticate';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders Authenticate page correctly', () => {
+  renderer.create(<Authenticate />);
 });
